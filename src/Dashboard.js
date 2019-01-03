610fa97ff9f24ff7282aab9276f3ceebf1e97813
Evan Procter <evanprocter@gmail.com>
2019-01-03 14:34:15 -0500

    added new tee time button

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { logoutUser } from './store';
+import { logoutUser, updateUser } from './store';
 
 export default function Dashboard(props) {
     return (
         <div className="Dashboard">
             <h1>{props.data.user.name}</h1>
            <input type="button" value="logout" onClick={event => {
-               logoutUser(props.data.user)
-           }}/>
+               logoutUser(props.data.user)}}/>
+            <input type="button" value="newTeeTime" onClick={event => {
+                const newTeeTime = new Date()
+                updateUser({...props.data.user, teeTimes: [...props.data.user.teeTimes, newTeeTime] })
+            }}
+           />
         </div>
     )
 }
