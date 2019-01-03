c4d843e7653aff57b96a8f0cd006879e0df69a98
Collin Argo <collinargo@gmail.com>
2019-01-03 13:33:49 -0500

    add request data function

---
+++ src/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dashboard(props) {
+    return (
+        <div className="Dashboard">
+            <h1>{props.data.user.name}</h1>
+            // add logout button
+        </div>
+    )
+}
