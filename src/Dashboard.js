a84b5b0e977bb80abebd210e72892cd931c46256
Evan Procter <evanprocter@gmail.com>
2019-01-03 14:08:14 -0500

    added logout button

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { logoutUser } from './store';
 
 export default function Dashboard(props) {
     return (
         <div className="Dashboard">
             <h1>{props.data.user.name}</h1>
             // add logout button
+           <a href="#" onClick={this.logoutUser()}>LOGOUT</a>
         </div>
     )
 }
