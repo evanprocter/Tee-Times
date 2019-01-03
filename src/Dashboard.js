66402adff437d776216207fef0ae7a39f7527379
Evan Procter <evanprocter@gmail.com>
2019-01-03 15:03:30 -0500

    added new tee time button but is not completely working

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,6 +5,8 @@ export default function Dashboard(props) {
     return (
         <div className="Dashboard">
             <h1>{props.data.user.name}</h1>
+            <div>Here is a list of your current and previous tee times!</div>
+            <ul>{props.data.user.newTeeTime}</ul>
            <input type="button" value="logout" onClick={event => {
                logoutUser(props.data.user)}}/>
             <input type="button" value="newTeeTime" onClick={event => {
