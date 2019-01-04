fd2cb0512fdd8b1bd22c181a71b1e5f51716e6e8
Evan Procter <evanprocter@gmail.com>
2019-01-04 11:27:07 -0500

    set min and max tee time dates and put them in a list

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,7 +7,7 @@ export default function Dashboard(props) {
         <div className="Dashboard">
             <h1>{props.data.user.name}</h1>
             <div>Here is a list of your current and previous tee times!</div>
-            <ul>{props.data.user.teeTimes.map(teeTime => teeTime.date)}</ul>
+            <ul>{props.data.user.teeTimes.map(teeTime => <li>{teeTime.date}</li>)}</ul>
             <input type="button" value="logout" onClick={event => {
                logoutUser(props.data.user)}}/>
                 <TeeTimeForm {...props}/>
--- src/TeeTimeForm.js
+++ src/TeeTimeForm.js
@@ -5,14 +5,15 @@ export default function TeeTimeForm(props) {
     const monthString = currentDate.getMonth() + 1 > 9 ? `${currentDate.getMonth() + 1}`: `0${currentDate.getMonth() + 1}`
     const dayString =  currentDate.getDate() > 9 ? `${currentDate.getDate()}` : `0${currentDate.getDate()}`
     const currentDateString = `${currentDate.getFullYear()}-${monthString}-${dayString}T${currentDate.getHours()}:${currentDate.getMinutes()}`
-    // const cutOffDateString = `${currentDate.getFullYear()}-0${currentDate.getMonth()+1}-0${currentDate.getDate() + 2}T${currentDate.getHours()}:${currentDate.getMinutes()}`
+    const cutOffDateString = `${currentDate.getFullYear()}-0${currentDate.getMonth()+1}-0${currentDate.getDate() + 2}T${currentDate.getHours()}:${currentDate.getMinutes()}`
     return (
         <form className="TeeTimeForm" onSubmit={event => {
             const teeDate = event.target.teeDate.value
                 const newTeeTime = new Date(teeDate)
                 props.addTeeTime(newTeeTime)
             }}>
-        <input type="datetime-local" name="teeDate" id="myDate" defaultValue={currentDateString}/>
+        <input type="datetime-local" name="teeDate" id="myDate" defaultValue={currentDateString} max={cutOffDateString} min={currentDateString}/>
+        <input type="submit" value="Request Tee Time"/>
         </form>
     )
 }
