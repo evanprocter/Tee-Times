74f6843c5b7602d5f5dc1f04b10ec75da5ee26a0
Evan Procter <evanprocter@gmail.com>
2019-01-04 11:41:40 -0500

    made it to where user can only check two days in advance

--- src/TeeTimeForm.js
+++ src/TeeTimeForm.js
@@ -4,8 +4,9 @@ export default function TeeTimeForm(props) {
     const currentDate= new Date()
     const monthString = currentDate.getMonth() + 1 > 9 ? `${currentDate.getMonth() + 1}`: `0${currentDate.getMonth() + 1}`
     const dayString =  currentDate.getDate() > 9 ? `${currentDate.getDate()}` : `0${currentDate.getDate()}`
+    const cutOffDayString =  currentDate.getDate() > 9 ? `${currentDate.getDate() + 2}` : `0${currentDate.getDate() + 2}`
     const currentDateString = `${currentDate.getFullYear()}-${monthString}-${dayString}T${currentDate.getHours()}:${currentDate.getMinutes()}`
-    const cutOffDateString = `${currentDate.getFullYear()}-${monthString}-${dayString + 2}T${currentDate.getHours()}:${currentDate.getMinutes()}`
+    const cutOffDateString = `${currentDate.getFullYear()}-${monthString}-${cutOffDayString}T${currentDate.getHours()}:${currentDate.getMinutes()}`
     return (
         <form className="TeeTimeForm" onSubmit={event => {
             event.preventDefault()
