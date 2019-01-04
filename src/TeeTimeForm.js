8af1190765202248547d879bc06c6436cad77111
Evan Procter <evanprocter@gmail.com>
2019-01-04 11:13:00 -0500

    showing current date to the tee time form

--- src/TeeTimeForm.js
+++ src/TeeTimeForm.js
@@ -2,25 +2,17 @@ import React from 'react'
 
 export default function TeeTimeForm(props) {
     const currentDate= new Date()
+    const monthString = currentDate.getMonth() + 1 > 9 ? `${currentDate.getMonth() + 1}`: `0${currentDate.getMonth() + 1}`
+    const dayString =  currentDate.getDate() > 9 ? `${currentDate.getDate()}` : `0${currentDate.getDate()}`
+    const currentDateString = `${currentDate.getFullYear()}-${monthString}-${dayString}T${currentDate.getHours()}:${currentDate.getMinutes()}`
+    // const cutOffDateString = `${currentDate.getFullYear()}-0${currentDate.getMonth()+1}-0${currentDate.getDate() + 2}T${currentDate.getHours()}:${currentDate.getMinutes()}`
     return (
         <form className="TeeTimeForm" onSubmit={event => {
             const teeDate = event.target.teeDate.value
                 const newTeeTime = new Date(teeDate)
                 props.addTeeTime(newTeeTime)
             }}>
-        <input type="date" name="teeDate" id="myDate" defaultValue={currentDate.toLocaleString()}/>
-
-        <select name="times">
-                <option value="one">8:00 am</option>
-                <option value="two">8:10 am</option>
-                <option value="three">8:20 am</option>
-                <option value="four">8:30 am</option>
-                <option value="five">8:40 am</option>
-                <option value="six">8:50 am</option>
-                <option value="seven">9:00 am</option>
-            </select>
-            <input type="submit" value="newTeeTime" 
-           />
+        <input type="datetime-local" name="teeDate" id="myDate" defaultValue={currentDateString}/>
         </form>
     )
 }
