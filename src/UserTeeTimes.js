60610163b6bef4bf353d189eed4dd8d85d9a1b46
Collin Argo <collinargo@gmail.com>
2019-01-07 18:43:40 -0500

    change ul to div for styles

--- src/UserTeeTimes.js
+++ src/UserTeeTimes.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function UserTeeTimes(props) {
     return (
-        <ul className='UserTeeTimes'>
+        <div className='UserTeeTimes'>
         {props.data.userTeeTimes.sort((teeTimeA, teeTimeB) => new Date(teeTimeA.date).getTime() > new Date(teeTimeB.date).getTime() ? -1 : 1)
             .map(teeTime => {
                 const editClassName = teeTime._id === props.selectedTeeTime._id ? ' teeTimeEdit' : ''
@@ -16,9 +16,15 @@ export default function UserTeeTimes(props) {
                     minute: 'numeric'
                 }
                 const dateString = new Intl.DateTimeFormat('en-US', dateOptions).format(teeDate)
-                return <li key={teeTime._id} className={`teeTime${editClassName}`} onClick={event => props.selectTeeTime(teeTime)}>{dateString}</li>
+                return (
+                    <div key={teeTime._id} className={`teeTime${editClassName}`} 
+                    onClick={event => props.selectTeeTime(teeTime)}>
+                        <p>{dateString}</p>
+                        <p>{teeTime.golfers.filter(golfer => golfer._id !== props.data.user._id).map(golfer => golfer.name).join(', ')}</p>
+                    </div>
+                )
             })
         }
-        </ul>
+        </div>
     )
 }
