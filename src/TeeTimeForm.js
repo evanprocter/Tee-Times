1f7416aaa900f2a5cf7e668cc14a0951371ee8cd
Collin Argo <collinargo@gmail.com>
2019-01-07 10:06:54 -0500

    fix date string format for tee time form

--- src/TeeTimeForm.js
+++ src/TeeTimeForm.js
@@ -4,9 +4,11 @@ export default function TeeTimeForm(props) {
     const currentDate= new Date()
     const monthString = currentDate.getMonth() + 1 > 9 ? `${currentDate.getMonth() + 1}`: `0${currentDate.getMonth() + 1}`
     const dayString =  currentDate.getDate() > 9 ? `${currentDate.getDate()}` : `0${currentDate.getDate()}`
+    const hourString = currentDate.getHours() > 9 ? `${currentDate.getHours()}` : `0${currentDate.getHours()}`
+    const minString = currentDate.getMinutes() > 9 ? `${currentDate.getMinutes()}` : `0${currentDate.getMinutes()}`
     const cutOffDayString =  currentDate.getDate() > 9 ? `${currentDate.getDate() + 2}` : `0${currentDate.getDate() + 2}`
-    const currentDateString = `${currentDate.getFullYear()}-${monthString}-${dayString}T${currentDate.getHours()}:${currentDate.getMinutes()}`
-    const cutOffDateString = `${currentDate.getFullYear()}-${monthString}-${cutOffDayString}T${currentDate.getHours()}:${currentDate.getMinutes()}`
+    const currentDateString = `${currentDate.getFullYear()}-${monthString}-${dayString}T${hourString}:${minString}`
+    const cutOffDateString = `${currentDate.getFullYear()}-${monthString}-${cutOffDayString}T${hourString}:${minString}`
     return (
         <form className="TeeTimeForm" onSubmit={event => {
                 event.preventDefault()
