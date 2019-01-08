ce1418758aa2ec023710d16beb97113c1cb08dc7
Collin Argo <collinargo@gmail.com>
2019-01-08 11:49:40 -0500

    add friend form

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
+import FriendForm from './FriendForm'
 
 export default function Profile(props) {
     return (
         <div className='Profile'>
             <NavBar {...props} />
-            
+            <FriendForm {...props}/>
         </div>
     )
 }
