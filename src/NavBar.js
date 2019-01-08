2c7944a6b1e19e634a4cca0b61e4a53ffb922267
Collin Argo <collinargo@gmail.com>
2019-01-08 17:00:19 -0500

    update link on profile page

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import {Switch, Route} from 'react-router-dom'
 
 export default function NavBar(props) {
     return (
         <div className='NavBar'>
             <h1 onClick={event => props.history.push('/')}>{`${props.data.user.name}${props.data.user.userType === 'admin' ? ' - admin' : ''}`}</h1>
             <input type="button" value="logout" onClick={event => props.logoutUser(props.data.user)}/>
-            <button onClick={() => props.history.push('/profile')}>profile page</button>
+            <Switch>
+                <Route exact path={'/profile'}
+                render={() => <button onClick={() => props.history.push('/')}>user dashboard</button>}/>
+                <Route path={'/'}
+                render={() => <button onClick={() => props.history.push('/profile')}>profile page</button>}/>
+            </Switch>
         </div>
     )
 }
