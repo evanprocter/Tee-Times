47270bef6b047c6191655ca193c82512fa593d34
Collin Argo <collinargo@gmail.com>
2019-01-08 09:49:24 -0500

    fix indents

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -3,23 +3,27 @@ import './App.css';
 
 export default function LoginPage({history, loginUser, addUser}) {
     return (
-            <form className="LoginPage" onSubmit={event => {
+        <form className="LoginPage" 
+            onSubmit={event => {
                 event.preventDefault()
                 loginUser(event.target.name.value, event.target.password.value)
                 history.push('/')
-            }}>
+            }}
+        >
             <label name='name'> 
                 Name:
                 <input type='text' name='name'/>
-                </label>
-                <label>
-                    Password:
-                    <input type='password' name='password' />
-                </label>
-                <input type='submit' value='login' />
-                <input type='button' value='register' onClick={event => {
-                    addUser(event.target.form[0].value, event.target.form[1].value)
-                    history.push('/')
-                    }}/>
-                    </form>
-                )}
+            </label>
+            <label>
+                Password:
+                <input type='password' name='password' />
+            </label>
+            <input type='submit' value='login' />
+            <input type='button' value='register' onClick={event => {
+                        addUser(event.target.form[0].value, event.target.form[1].value)
+                        history.push('/')
+            }}/>
+            <inpu
+        </form>
+    )
+}
