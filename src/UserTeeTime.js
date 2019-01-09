311957d5853dc22388196c22531ce9afb506ebee
Evan Procter <evanprocter@gmail.com>
2019-01-09 14:47:51 -0500

    split future and previous tee times and removed edit button from past tee times

---
+++ src/UserTeeTime.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function UserTeeTime(props) {
+        const {teeTime, isPast} = props
+        const editClassName = teeTime._id === props.selectedTeeTime._id ? ' teeTimeEdit' : ''
+        const teeDate = new Date(teeTime.date)
+        const dateOptions = {
+            year: 'numeric', 
+            month: 'short', 
+            day: 'numeric',
+            weekday: 'short',
+            hour: 'numeric',
+            minute: 'numeric'
+        }
+        const dateString = new Intl.DateTimeFormat('en-US', dateOptions).format(teeDate)
+        return (
+            <div key={teeTime._id} className={`teeTime${editClassName}`} 
+            onClick={isPast ? null : event => props.selectTeeTime(teeTime)}>
+                <p>{dateString}</p>
+                <p>{teeTime.teeType}</p>
+                <p>{teeTime.golfers.filter(golfer => props.data.user.userType === 'admin' || golfer._id !== props.data.user._id).map(golfer => golfer.name).join(', ')}</p>
+                {props.selectedTeeTime._id === teeTime._id && <input type="button" value="Remove Tee Time" onClick={event => props.deleteTeeTime(props.selectedTeeTime)}/>}
+            </div>
+        
+    )
+}
--- src/UserTeeTimes.js
+++ src/UserTeeTimes.js
@@ -1,34 +1,18 @@
 import React from 'react'
+import UserTeeTime from './UserTeeTime'
 
 export default function UserTeeTimes(props) {
+    const currentDate = new Date()
     const userTeeTimes = props.data.user.userType === 'admin' ? props.data.allTeeTimes : props.data.userTeeTimes
+    userTeeTimes.sort((teeTimeA, teeTimeB) => new Date(teeTimeA.date).getTime() > new Date(teeTimeB.date).getTime() ? -1 : 1)
+    const futureTeeTimes = userTeeTimes.filter(teeTime => new Date(teeTime.date) > currentDate)
+    const pastTeeTimes = userTeeTimes.filter(teeTime => new Date(teeTime.date) <= currentDate)
     return (
         <div className='UserTeeTimes'>
-            <h4>Here is a list of your current and previous tee times!</h4>
-            {userTeeTimes.sort((teeTimeA, teeTimeB) => new Date(teeTimeA.date).getTime() > new Date(teeTimeB.date).getTime() ? -1 : 1)
-                .map(teeTime => {
-                    const editClassName = teeTime._id === props.selectedTeeTime._id ? ' teeTimeEdit' : ''
-                    const teeDate = new Date(teeTime.date)
-                    const dateOptions = {
-                        year: 'numeric', 
-                        month: 'short', 
-                        day: 'numeric',
-                        weekday: 'short',
-                        hour: 'numeric',
-                        minute: 'numeric'
-                    }
-                    const dateString = new Intl.DateTimeFormat('en-US', dateOptions).format(teeDate)
-                    return (
-                        <div key={teeTime._id} className={`teeTime${editClassName}`} 
-                        onClick={event => props.selectTeeTime(teeTime)}>
-                            <p>{dateString}</p>
-                            <p>{teeTime.teeType}</p>
-                            <p>{teeTime.golfers.filter(golfer => props.data.user.userType === 'admin' || golfer._id !== props.data.user._id).map(golfer => golfer.name).join(', ')}</p>
-                            {props.selectedTeeTime._id === teeTime._id && <input type="button" value="Remove Tee Time" onClick={event => props.deleteTeeTime(props.selectedTeeTime)}/>}
-                        </div>
-                    )
-                })
-            }
+            <h4>Here is a list of your upcoming tee times!</h4>
+            {futureTeeTimes.map(teeTime => <UserTeeTime isPast={false} teeTime={teeTime}{...props}/>)}
+            <h4>Here are your completed tee times!</h4>
+            {pastTeeTimes.map(teeTime =>  <UserTeeTime isPast={true} teeTime={teeTime}{...props}/>)}
         </div>
     )
 }
