617a83f7473875d9be84f11f68f81684691a2878
Evan Procter <evanprocter@gmail.com>
2019-01-10 17:39:55 -0500

    compared tee time dats

--- src/UserTeeTimes.js
+++ src/UserTeeTimes.js
@@ -3,26 +3,32 @@ import UserTeeTime from './UserTeeTime'
 
 export default function UserTeeTimes(props) {
     const currentDate = new Date()
-    const userTeeTimes = props.data.user.userType === 'admin' ? props.data.allTeeTimes : props.data.userTeeTimes
+    let userTeeTimes = props.data.user.userType === 'admin' ? props.data.allTeeTimes : props.data.userTeeTimes
     userTeeTimes.sort((teeTimeA, teeTimeB) => new Date(teeTimeA.date).getTime() > new Date(teeTimeB.date).getTime() ? -1 : 1)
-    props.isSearching && userTeeTimes.filter(teeTime => {
+    userTeeTimes = props.isSearching ? userTeeTimes.filter(teeTime => {
         for (let field in props.teeTimeSearch) {
-            console.log(new Date(teeTime[field]))
-            console.log(props.teeTimeSearch[field])
+            console.log(field)
             if (field === 'date') {
-                if (new Date(teeTime[field]) !== props.teeTimeSearch[field]) {return false}
+                const teeTimeDate = new Date(teeTime[field])
+                teeTimeDate.setSeconds(0)
+                teeTimeDate.setMilliseconds(0)
+                if (teeTimeDate.getTime() !== props.teeTimeSearch[field].getTime()) {return false}
             } else if (field === 'golfers') {
-
+                console.log(teeTime[field], props.teeTimeSearch[field])
+                const matchingGolfers = teeTime[field].filter(golfer => props.teeTimeSearch[field].map(golfer => golfer._id).includes(golfer._id))
+                if (matchingGolfers.length === teeTime[field].length) {return false}
             } else {
+                console.log(teeTime[field], props.teeTimeSearch[field])
                 if (teeTime[field] !== props.teeTimeSearch[field]) {return false}
             }
         }
+        console.log("tee times match")
         return true
-    })
+    }) : userTeeTimes
     const futureTeeTimes = userTeeTimes.filter(teeTime => new Date(teeTime.date) > currentDate)
     const pastTeeTimes = userTeeTimes.filter(teeTime => new Date(teeTime.date) <= currentDate)
     return (
-        <div className='UserTeeTimes'>
+        <div className={`UserTeeTimes${props.isSearching ? ' searchingTeeTimes' : ''}`}>
             <input type="button" value="Tee Time Search" onClick={props.searchTeeTimes}/>
             <h4>Here is a list of your upcoming tee times!</h4>
             {futureTeeTimes.map(teeTime => <UserTeeTime key={teeTime._id} isPast={false} teeTime={teeTime}{...props}/>)}
