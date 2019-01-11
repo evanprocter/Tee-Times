22aead14f4fd18e964fd0f861cb40c98e85486cc
Collin Argo <collinargo@gmail.com>
2019-01-11 12:05:11 -0500

    deselect tee time when returning home

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,10 @@ import {NavLink} from 'react-router-dom'
 export default function NavBar(props) {
     return (
         <div className='NavBar'>
-            <h1 onClick={event => props.history.push('/')}>{`${props.data.user.name}${props.data.user.userType === 'admin' ? ' - admin' : ''}`}</h1>
+            <h1 onClick={event => {
+                props.selectTeeTime(props.selectedTeeTime)
+                props.history.push('/')
+            }}>{`${props.data.user.name}${props.data.user.userType === 'admin' ? ' - admin' : ''}`}</h1>
             <input type="button" value="logout" onClick={event => props.logoutUser(props.data.user)}/>
             <NavLink className="navlink" to='/teetimes'>Tee Times</NavLink>
             <NavLink className="navlink" to='/friends'>Friends</NavLink>
