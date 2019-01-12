a8f07296fbc15786dcd63bc9254d0d3078b7bc14
Collin Argo <collinargo@gmail.com>
2019-01-12 10:35:54 -0500

    style home link background

--- src/Profile.js
+++ src/Profile.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Profile(props) {
     return (
         <div className='Profile'>
-            
+            {/* should change password, username, profile pic */}
         </div>
     )
 }
