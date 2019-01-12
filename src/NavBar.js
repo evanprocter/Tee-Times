89b390ef999d8b69a5ef96c519074c796a777e2f
Collin Argo <collinargo@gmail.com>
2019-01-12 09:48:16 -0500

    add link to profile page

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,10 +4,13 @@ import {NavLink} from 'react-router-dom'
 export default function NavBar(props) {
     return (
         <div className='NavBar'>
-            <h1 onClick={event => {
+            <img onClick={event => {
                 props.selectTeeTime(props.selectedTeeTime)
                 props.history.push('/')
-            }}>{`${props.data.user.name}${props.data.user.userType === 'admin' ? ' - admin' : ''}`}</h1>
+            }}/>
+            <NavLink className='navlink' to='/profile'>
+                {`${props.data.user.name}${props.data.user.userType === 'admin' ? ' - admin' : ''}`}
+            </NavLink>
             <input type="button" value="logout" onClick={event => props.logoutUser(props.data.user)}/>
             <NavLink className="navlink" to='/teetimes'>Tee Times</NavLink>
             <NavLink className="navlink" to='/friends'>Friends</NavLink>
