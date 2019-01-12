8e2feca542f56b4ab17ac08e6ed6a1db8934bb94
Collin Argo <collinargo@gmail.com>
2019-01-12 10:51:40 -0500

    correct indent

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -13,43 +13,43 @@ export default function Dashboard(props) {
         <div className="Dashboard">
             <NavBar {...props} />
             {/* <Switch> */}
-                <Route exact path={'/'} render= {routeProps => {
-                    const currentDate = new Date()
-                    props.data.userTeeTimes.sort((teeTimeA, teeTimeB) => new Date(teeTimeA.date).getTime() < new Date(teeTimeB.date).getTime() ? -1 : 1)
-                    const upcomingTeeTime = props.data.userTeeTimes.find(teeTime => new Date(teeTime.date) > currentDate)
-                    const upcomingTeeTimeMessage = upcomingTeeTime ? `Your next upcoming tee time is:` : "You do not have any current upcoming tee times. Press tee times to add a new one!"
-                    
-                    const friendRequests = props.data.allUsers.filter(user => props.data.user.friendRequests.includes(user._id))
-                    const friendRequestsMessage = friendRequests.length > 0 ? `You have ${friendRequests.length} new friend requests.` : 'You have no new friend requests.'
-                    return(
-                        <div>
-                            <h4>{upcomingTeeTimeMessage}</h4>
-                            {upcomingTeeTime && <UserTeeTime teeTime={upcomingTeeTime} {...props}/>}
-                            <h4>{friendRequestsMessage}</h4>
-                            {friendRequests && friendRequests.map(friend => <Friend key={friend._id} golfer={friend} {...props}/>)}
-                        </div>
-                    )
-                }}/>
-                <Route exact path={'/profile'} render= {routeProps => {
-                    return(
-                        <div>
-                            <Profile {...props}/>
-                        </div>
-                    )
-                }}/>
-                <Route exact path={'/teetimes'} render= {routeProps => {
-                    return(
-                        <div>
-                            <TeeTimeForm {...props}/>
-                            <UserTeeTimes {...props}/>
-                        </div>
-                    )
-                }}/>
-                <Route exact path={'/friends'} render= {routeProps => {
-                    return(
-                        <Friends {...props}/>
-                    )
-                }}/>
+            <Route exact path={'/'} render= {routeProps => {
+                const currentDate = new Date()
+                props.data.userTeeTimes.sort((teeTimeA, teeTimeB) => new Date(teeTimeA.date).getTime() < new Date(teeTimeB.date).getTime() ? -1 : 1)
+                const upcomingTeeTime = props.data.userTeeTimes.find(teeTime => new Date(teeTime.date) > currentDate)
+                const upcomingTeeTimeMessage = upcomingTeeTime ? `Your next upcoming tee time is:` : "You do not have any current upcoming tee times. Press tee times to add a new one!"
+                
+                const friendRequests = props.data.allUsers.filter(user => props.data.user.friendRequests.includes(user._id))
+                const friendRequestsMessage = friendRequests.length > 0 ? `You have ${friendRequests.length} new friend requests.` : 'You have no new friend requests.'
+                return(
+                    <div>
+                        <h4>{upcomingTeeTimeMessage}</h4>
+                        {upcomingTeeTime && <UserTeeTime teeTime={upcomingTeeTime} {...props}/>}
+                        <h4>{friendRequestsMessage}</h4>
+                        {friendRequests && friendRequests.map(friend => <Friend key={friend._id} golfer={friend} {...props}/>)}
+                    </div>
+                )
+            }}/>
+            <Route exact path={'/profile'} render= {routeProps => {
+                return(
+                    <div>
+                        <Profile {...props}/>
+                    </div>
+                )
+            }}/>
+            <Route exact path={'/teetimes'} render= {routeProps => {
+                return(
+                    <div>
+                        <TeeTimeForm {...props}/>
+                        <UserTeeTimes {...props}/>
+                    </div>
+                )
+            }}/>
+            <Route exact path={'/friends'} render= {routeProps => {
+                return(
+                    <Friends {...props}/>
+                )
+            }}/>
             {/* </Switch> */}
         </div>
     )
