ef632a607d08e67d9502bda694e02fb0c69cb2b0
Collin Argo <collinargo@gmail.com>
2019-01-12 13:09:12 -0500

    add default user image to register

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import defaultUserImage from './golf_ball.png'
 
 export default function LoginPage(props) {
     return (
@@ -20,14 +21,14 @@ export default function LoginPage(props) {
             <input type='button' value='register' 
                 onClick={event => {
                     if (!props.data.allUsers.map(user => user.name).includes(event.target.form[0].value)) {
-                        props.addUser(event.target.form[0].value, event.target.form[1].value, 'basic')
+                        props.addUser(event.target.form[0].value, event.target.form[1].value, defaultUserImage, 'basic')
                     }
                 }}
             />
             <input type='button' value='register as admin' 
                 onClick={event => {
                     if (!props.data.allUsers.map(user => user.name).includes(event.target.form[0].value)) {
-                        props.addUser(event.target.form[0].value, event.target.form[1].value, 'admin')
+                        props.addUser(event.target.form[0].value, event.target.form[1].value, defaultUserImage, 'admin')
                     }
                 }}
             />
