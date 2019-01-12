13cbca74033ce51500258afc5ca73e63509f14e2
Collin Argo <collinargo@gmail.com>
2019-01-12 09:26:37 -0500

    remove loading page

--- src/LoadingPage.js
+++ src/LoadingPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoadingPage(props) {
     return (
         <div className='LoadingPage'>
-            <img src='./tee_shot.gif' alt='taking a shot' />
+            {/* <img src='./tee_shot.gif' alt='taking a shot' /> */}
         </div>
     )
 }
