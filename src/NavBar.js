1da42430c4d207552e69b2e3b393ff7d60621d9c
Collin Argo <collinargo@gmail.com>
2019-01-13 14:32:12 -0500

    change home icon

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar(props) {
     return (
         <div className='NavBar'>
             <NavLink className='navlink' exact to='/'>
-                <img src='./tee-ball.png' alt='a golf ball on a tee'/>
+                <img src='./tee_hole.png' alt='a golf ball on a tee'/>
             </NavLink>
             <NavLink className='navlink' to='/profile'>
                 <img src={props.data.user.pictureSrc} alt='user profile'/>
