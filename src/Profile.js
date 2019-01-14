087a547228107870c02fff40db285f5f921736aa
Collin Argo <collinargo@gmail.com>
2019-01-13 22:59:32 -0500

    try weak map

--- src/Profile.js
+++ src/Profile.js
@@ -3,6 +3,17 @@ import React, {Component} from 'react'
 // import ReactDOM from 'react-dom'
 // import Root from './Root';
 // import store from './store';
+let urls = new WeakMap()
+
+let blobUrl = blob => {
+  if (urls.has(blob)) {
+    return urls.get(blob)
+  } else {
+    let url = URL.createObjectURL(blob)
+    urls.set(blob, url)
+    return url
+  }
+}
 
 export default class Profile extends Component {
     constructor(props) {
@@ -12,17 +23,17 @@ export default class Profile extends Component {
             newPassword: '',
             usernameSearchTerm: '',
             resizingPicture: false,
-            blobURL: ''
+            myImageFile: null,
         }
     }
 
-    componentDidUpdate() {
-        this.state.resizingPicture && 
-        this.setState({
-            resizingPicture: false,
-            blobURL: ''
-        })
-    }
+    // componentDidUpdate() {
+    //     this.state.resizingPicture && 
+    //     this.setState({
+    //         resizingPicture: false,
+    //         blobURL: ''
+    //     })
+    // }
 
     render() {
     const {props} = this
@@ -39,7 +50,7 @@ export default class Profile extends Component {
             <input type='submit' value='submit'/>
             <input type='reset' value='reset'/>
         </div>
-    )
+    )    
     return (
         <form className='Profile'
             onSubmit={event => {
@@ -47,41 +58,27 @@ export default class Profile extends Component {
                 const newUsername = event.target.newUsername.value
                 const currentPassword = event.target.currentPassword.value
                 const newPassword = event.target.newPassword.value
-                const fr = new FileReader()
-                fr.onload = () => {
-                    this.setState({
-                        resizingPicture: true,
-                    }, () => {
-                        const myCanvas = document.getElementById('myCanvas')
-                        // createImageBitmap(new Blob([fr.result]))
-                        // .then(myImageBitmap => {
-                            const myImage = new Image(100, 100)
-                            myImage.src = URL.createObjectURL(new Blob([fr.result]))
-                            const canvasContext = myCanvas.getContext('2d')
-                            // console.log(myImageBitmap)
-                            // draw image takes (img, x, y, w, h)
-                            canvasContext.drawImage(myImage, 0, 0, 100, 100)
-                            console.log(canvasContext)
-                            myCanvas.toBlob((imageBlob) => {
-                                const frBlob = new FileReader()
-                                frBlob.onload = () => {
-                                    // update the user
-                                    props.updateUser({
-                                        _id: props.data.user._id,
-                                        name: props.data.user.name,
-                                        newUsername,
-                                        currentPassword,
-                                        newPassword,
-                                        newPicture: frBlob.result
-                                    })
-                                }
-                                frBlob.readAsBinaryString(imageBlob)
-                            }, 'image/png', .5)
+                const myCanvas = document.getElementById('myCanvas')
+                const myImage = document.getElementById('profilePicture')
+                const canvasContext = myCanvas.getContext('2d')
+                canvasContext.drawImage(myImage, 0, 0, 100, 100)
+                myCanvas.toBlob((imageBlob) => {
+                    const fr = new FileReader()
+                    fr.onload = () => {
+                        // update the user
+                        props.updateUser({
+                            _id: props.data.user._id,
+                            name: props.data.user.name,
+                            newUsername,
+                            currentPassword,
+                            newPassword,
+                            newPicture: fr.result
                         })
-                    // })
-                }
+                    }
+                    fr.readAsBinaryString(imageBlob)
+                }, 'image/png', .5)
                 event.target.newPicture.files[0] ?
-                    fr.readAsArrayBuffer(event.target.newPicture.files[0]) :
+                    this.setState({resizingPicture: true, myImageFile: event.target.newPicture.files[0]}) :
                     props.updateUser({
                         _id: props.data.user._id,
                         name: props.data.user.name,
@@ -107,11 +104,19 @@ export default class Profile extends Component {
             <div className='changePicture'>
                 <label>
                     Change picture:
-                    {this.state.resizingPicture ? <canvas id='myCanvas' ref='myCanvas' width={100} height={100}/> :
-                    <img src={props.data.user.pictureSrc} alt={`user's profile`}/>}
+                    {/* {this.state.resizingPicture ?  */}
+                    {/* (<> */}
+                    <canvas id='myCanvas' ref='myCanvas' width={100} height={100}/>
+                    {/* <img id='myImage' src={this.state.myImageDataURL} alt='uploaded profile'/></> */}
+                    <img id='profilePicture' 
+                    src={this.state.file ? 
+                        blobUrl(this.state.file) : 
+                        this.props.data.user.pictureSrc} 
+                    alt={`user's profile`}/>
+                    {/* } */}
                     <input 
                         type='file' name='newPicture' accept='image/*' 
-                        onChange={() => this.setState({pictureUploaded: true})}
+                        onChange={event => this.setState({pictureUploaded: true, myImage: event.target.files[0]})}
                         onFocus={() => {
                             this.setState({
                                 usernameSearchTerm: '',
